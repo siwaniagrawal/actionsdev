61b5784cbceb0174e87ec026474c3a589c649457
siwaniagrawal <siwaniagrawal18@gmail.com>
2018-08-03 17:12:57 +0530

    second commit

--- level2/functions/index.js
+++ level2/functions/index.js
@@ -40,36 +40,24 @@ app.intent('Symptoms', (conv, { symptoms }) => {
   const luckyNumber = symptoms.length;
   console.log(symptoms);
   const audioSound = 'https://actions.google.com/sounds/v1/cartoon/clang_and_wobble.ogg';
-  if (conv.data.userName) {
-    // If we collected user name previously, address them by name and use SSML
-    // to embed an audio snippet in the response.
-    conv.ask(`<speak>${conv.data.userName}, your lucky number is ` +
-      `${luckyNumber}.<audio src="${audioSound}"></audio>` +
-      `Would you like to hear some fake colors?</speak>`);
-  } else {
+ 
     console.log(symptoms);
     conv.ask(`<speak>Your lucky number is ${symptoms}.` +
       `<audio src="${audioSound}"></audio>` +
       `Would you like to hear some fake colors?</speak>`);
-  }
-});
+  
+})
 
 app.intent('Diseases', (conv, { disease }) => {
   const luckyNumber = disease.length;
   console.log(disease);
   const audioSound = 'https://actions.google.com/sounds/v1/cartoon/clang_and_wobble.ogg';
-  if (conv.data.userName) {
-    // If we collected user name previously, address them by name and use SSML
-    // to embed an audio snippet in the response.
-    conv.ask(`<speak>${conv.data.userName}, your lucky number is ` +
-      `${disease}.<audio src="${audioSound}"></audio>` +
-      `Would you like to hear some fake colors?</speak>`);
-  } else {
+   
     console.log(disease);
     conv.ask(`<speak>Your disease is ${disease}.` +
       `<audio src="${audioSound}"></audio>` +
       `Would you like to hear some fake colors?</speak>`);
-  }
+  
 });
 
 // Define a mapping of fake color strings to basic card objects.
@@ -109,7 +97,7 @@ app.intent('Diseases', (conv, { disease }) => {
 // Set the DialogflowApp object to handle the HTTPS POST request.
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
 // Handle the Dialogflow intent named 'Default Welcome Intent'.
-app.intent('Default Welcome Intent', (conv) => {
+/*app.intent('Default Welcome Intent', (conv) => {
   conv.ask(new Permission({
     context: 'Hi there, to get to know you better',
     permissions: 'NAME'
@@ -124,4 +112,4 @@ app.intent('actions_intent_PERMISSION', (conv, params, permissionGranted) => {
     conv.data.userName = conv.user.name.display;
     conv.ask(`Thanks, ${conv.data.userName}. What's your favorite color?`);
   }
-});
+});*/
